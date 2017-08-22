702d6889ceeae118fd30068a0ae088148e323d47
Daniel Koza <dakolech@gmail.com>
2017-08-22 14:25:27 +0200

    upgrade lint script, upgrade tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,8 +2,15 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpWrapper } from '@briisk/http-wrapper';
 
 describe('AppComponent', () => {
+  const mockHttpWrapper = {
+    setBaseUrl: jasmine.createSpy('setBaseUrl'),
+    setHeader: jasmine.createSpy('setHeader'),
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -12,6 +19,10 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
       ],
+      schemas: [ NO_ERRORS_SCHEMA ],
+      providers: [
+        { provide: HttpWrapper, useValue: mockHttpWrapper },
+      ],
     }).compileComponents();
   }));
 
@@ -19,18 +30,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
   }));
 });
--- src/app/counter/components/show/show.component.spec.ts
+++ src/app/counter/components/show/show.component.spec.ts
@@ -1,14 +1,26 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ShowComponent } from './show.component';
+import { Store } from '@ngrx/store';
+import { CounterState } from '../../counter.reducer';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 describe('ShowComponent', () => {
   let component: ShowComponent;
   let fixture: ComponentFixture<ShowComponent>;
+  const counterState: CounterState = { value: 3 };
+  const counterStore$ = new BehaviorSubject<CounterState>(counterState);
+  const mockStore = {
+    select: jasmine.createSpy('select').and.returnValue(counterStore$),
+    dispatch: () => {},
+  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ShowComponent ],
+      providers: [
+        { provide: Store, useValue: mockStore },
+      ],
     })
     .compileComponents();
   }));
