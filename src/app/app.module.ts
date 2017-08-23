3bb6829114296ffa2cb30650be8c617874a83ced
Daniel Koza <dakolech@gmail.com>
2017-08-23 12:16:22 +0200

    add local storage sync

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
+import { ActionReducer, StoreModule } from '@ngrx/store';
 import { counterReducer, CounterState } from './counter/counter.reducer';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { CounterModule } from './counter/counter.module';
@@ -11,12 +11,19 @@ import { CounterEffects } from './counter/counter.effects';
 import { EffectsModule } from '@ngrx/effects';
 import { routerReducer, StoreRouterConnectingModule } from '@ngrx/router-store';
 import { HttpWrapperModule } from '@briisk/http-wrapper';
+import { localStorageSync } from 'ngrx-store-localstorage';
 
 import 'rxjs/add/operator/mapTo';
 
 export interface AppState {
   counter: CounterState;
 }
+
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({ keys: ['counter'], rehydrate: true })(reducer);
+}
+const metaReducers = [ localStorageSyncReducer ];
+
 
 @NgModule({
   declarations: [
@@ -27,10 +34,15 @@ export interface AppState {
     AppRoutingModule,
     CounterModule,
     HttpWrapperModule,
-    StoreModule.forRoot({
-      router: routerReducer,
-      counter: counterReducer,
-    }),
+    StoreModule.forRoot(
+      {
+        router: routerReducer,
+        counter: counterReducer,
+      },
+      {
+        metaReducers,
+      },
+    ),
     EffectsModule.forRoot([ CounterEffects ]),
     StoreRouterConnectingModule,
     StoreDevtoolsModule.instrument(),
